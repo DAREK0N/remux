ad93c48bc3132d3da1fd31a4ea3b818d3400f0fd
DAREKON <63023042+DAREK0N@users.noreply.github.com>
2025-08-23 23:43:59 +0200

    fluff

--- src/components/settings_sidebar.rs
+++ src/components/settings_sidebar.rs
@@ -1,13 +1,35 @@
 use dioxus::prelude::*;
-use crate::{components::{SettingsCategory, SettingsItem}, Route};
+use crate::{components::{SettingsCategory, SettingsItem}, hooks, Route};
 
 #[component]
 pub fn SettingsSidebar() -> Element {
+    let mut server = hooks::use_server();
+    let mut config = hooks::use_server_config();
+
     rsx! {
         div { 
-            class: "min-h-screen w-100 bg-gray-700/20 mx-5 my-5 flex flex-col rounded-md gap-1 items-center",
-            h1 { class: "self-center text-2xl mt-2",
-                "Settings"
+            class: "min-h-screen w-80 min-w-80 bg-gray-700/20 mx-5 my-5 flex flex-col rounded-md gap-1 items-center",
+            div {
+                class: "flex flex-row content-between mt-2 mb-4 gap-12 items-center",
+                Link {
+                    class: "cursor-pointer select-none",
+                    to: Route::Home {  },
+                    "Back"
+                }
+                h1 { class: "text-2xl select-none",
+                    "Settings"
+                }
+                a {
+                    onclick: {
+                        move |_| {
+                            config.set(None);
+                            server.set(None);
+                        }
+                    },
+                    class: "cursor-pointer select-none text-red-900/50",
+                    href: "/login",
+                    "Logout"
+                    }
             }
             div {
                 class: "w-[95%]",
